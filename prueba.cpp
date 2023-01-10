aae92805cc50883fb6b9abaac5e0cb0cccec7395
Carlos <57544836+carmoran0@users.noreply.github.com>
2023-01-10 09:03:22 +0000

    Pending changes exported from your codespace

---
+++ prueba.cpp
@@ -0,0 +1,13 @@
+//Instrucctones basicas
+#include<iostream>//Librerta de cabecera
+using namespace std;
+
+int main(){
+    //definimos 3 variables
+    int x,y,z;
+    cout<<"introduce tres variables: ";
+    cin>> x >> y >> z;
+    cout<<"Has introducido las variables";
+    cout<< x << y << z << endl;
+    return 0;
+}
